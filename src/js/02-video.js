6ecdc8878c38f3177921523044a6f453dc45c047
TarasDer <tderev.switch@gmail.com>
2023-03-13 23:56:35 +0200

    редагує завдання 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,31 +11,36 @@ let formData = {};
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
+populateForm();
+
+function onFormSubmit(ev) {
+  ev.preventDefault();
+
+  if (textareaEl.value === '' || inputEl.value === '') {
+    return alert('Заповніть всі поля');
+  }
+
+  console.log(formData);
+
+  ev.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
 function onFormInput(ev) {
   formData[ev.target.name] = ev.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-populateForm();
-
 function populateForm() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData.email) {
+    inputEl.value = savedData.email;
+    formData.email = savedData.email;
+  }
 
   if (savedData.message) {
     textareaEl.value = savedData.message;
     formData.message = savedData.message;
   }
-  if (savedData.email) {
-    inputEl.value = savedData.email;
-    formData.email = savedData.email;
-  }
-}
-
-function onFormSubmit(ev) {
-  ev.preventDefault();
-  console.log(formData);
-  if (textareaEl.value !== '' && inputEl.value !== '') {
-    ev.target.reset();
-    localStorage.removeItem(STORAGE_KEY);
-  } else alert('Заповніть всі поля');
 }
