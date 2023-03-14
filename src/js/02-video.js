01d4e0739120dd785b39a191d8b0afbc694d32a2
TarasDer <tderev.switch@gmail.com>
2023-03-15 00:13:03 +0200

    Додає завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(playerTimeUpdate, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function playerTimeUpdate(data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
